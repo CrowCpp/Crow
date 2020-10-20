ff9bd081ab480e6debe099d0f2f91914f6677ba8
Travis CI <travis@travis-ci.org>
2020-10-20 06:45:05 +0000

    Deploy code docs to GitHub Pages Travis build: 38
    Commit: 8a99167d5982b33cc4fe0b49a3b8cc9a165fc9e3

--- html/classcrow_1_1_connection.js
+++ html/classcrow_1_1_connection.js
@@ -7,5 +7,5 @@ var classcrow_1_1_connection =
     [ "handle_header", "classcrow_1_1_connection.html#adde7a01b5ad446b0f7b1fb6fa6d3c23b", null ],
     [ "socket", "classcrow_1_1_connection.html#adb5878dd700c30dae10f3fa8d06ecbab", null ],
     [ "start", "classcrow_1_1_connection.html#a00e6802466a18fd967ce065522844e2f", null ],
-    [ "crow::response", "classcrow_1_1_connection.html#aef2994c02fdd7787994f4465231da94b", null ]
+    [ "crow::response", "classcrow_1_1_connection.html#a6fdaf752fa7f9477577ad34e5a2b1506", null ]
 ];
