d8fceb8d786c716c84b42a02d4baa26085aee624
Farook Al-Sammarraie <farook@the-e-dev.com>
2021-06-28 12:51:25 +0300

    fixed problem in app.h

--- include/crow/app.h
+++ include/crow/app.h
@@ -1,7 +1,7 @@
+/*
 All rights reserved.
               2020-2021, CrowCpp
 Copyright (c) 2014-2017, ipkn
-/*
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
