4f4135a6b80f19a8196b7bad62d60d91c406eca7
The-EDev <farook@the-e-dev.com>
2021-06-28 12:59:08 +0300

    replaced 0.3 with master in master branch this is to more accurately distinguish where a crow build is coming from (it used to be the latest release, which is not the best idea)

--- include/crow/http_server.h
+++ include/crow/http_server.h
@@ -26,7 +26,7 @@ namespace crow
     class Server
     {
     public:
-    Server(Handler* handler, std::string bindaddr, uint16_t port, std::string server_name = "Crow/0.3", std::tuple<Middlewares...>* middlewares = nullptr, uint16_t concurrency = 1, typename Adaptor::context* adaptor_ctx = nullptr)
+    Server(Handler* handler, std::string bindaddr, uint16_t port, std::string server_name = "Crow/master", std::tuple<Middlewares...>* middlewares = nullptr, uint16_t concurrency = 1, typename Adaptor::context* adaptor_ctx = nullptr)
             : acceptor_(io_service_, tcp::endpoint(boost::asio::ip::address::from_string(bindaddr), port)),
             signals_(io_service_, SIGINT, SIGTERM),
             tick_timer_(io_service_),
