80e4d3f88fb57eb50aebf21e121ba944afdc177a
Lucas David <lucas.david@actemium.com>
2021-08-16 14:22:23 +0200

    - Removed duplicated in example.

--- examples/example_json_map.cpp
+++ examples/example_json_map.cpp
@@ -33,25 +33,6 @@ CROW_ROUTE(app, "/json-initializer-list-constructor")
   });
 });
 
-CROW_ROUTE(app, "/json-map-constructor")
-([] {
-  crow::json::wvalue::object_type map({
-    {"first", "Hello world!"},        /* stores a char const* hence a type::String */
-    {"second", "How are you today?"}, /* stores a char const* hence a std::int64_t. */
-    {"third", 54},      /* stores an int (as 54 is an int literal) hence a std::int64_t. */
-    {"fourth", 54l},    /* stores a long (as 54l is a long literal) hence a std::int64_t. */
-    {"fifth", 54u},     /* stores an unsigned int (as 54u is a unsigned int literal) hence a std::uint64_t. */
-    {"sixth", 54ul},    /* stores an unsigned long (as 54ul is an unsigned long literal) hence a std::uint64_t. */
-    {"seventh", 2.f},   /* stores a float (as 2.f is a float literal) hence a double. */
-    {"eighth", 2.},     /* stores a double (as 2. is a double literal) hence a double. */
-    {"ninth", nullptr}, /* stores a std::nullptr hence type::Null . */
-    {"tenth", true}     /* stores a bool hence type::True . */
-  });
-
-   return crow::json::wvalue(map);
-});
-
-
 // enables all log
 app.loglevel(crow::LogLevel::Debug);
 
