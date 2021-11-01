ed8d1ebaaa4e50058bb1c5a4b2871b66cbbedcc3
The-EDev <farook@the-e-dev.com>
2021-11-01 12:02:25 +0300

    Added checks for GET on HTTP/0.9 and any other errors Earlier behavior only checked whether every byte was parsed, which isn't ideal
    Signed-off-by: The-EDev <farook@the-e-dev.com>

--- include/crow/parser.h
+++ include/crow/parser.h
@@ -112,6 +112,10 @@ namespace crow
             };
 
             int nparsed = http_parser_execute(this, &settings_, buffer, length);
+            if (http_errno != HPE_OK)
+            {
+                return false;
+            }
             return nparsed == length;
         }
 
