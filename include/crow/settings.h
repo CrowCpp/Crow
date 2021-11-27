7ee13c4536a87cda1f35fbf89b238327056c1a05
Florian Rupprecht <floruppr@gmail.com>
2021-11-27 14:30:37 +0100

    GCC 8 workaround compiler detection & error

--- include/crow/settings.h
+++ include/crow/settings.h
@@ -58,10 +58,10 @@
 #endif
 #endif
 
-#if __GNUC__ == 8 && __GNUC_MINOR__ < 4
+#if defined(__GNUC__) && __GNUC__ == 8 && __GNUC_MINOR__ < 4
 #if __cplusplus > 201103L
 #define CROW_GCC83_WORKAROUND
 #else
-#warning "GCC 8.1 - 8.3 has a bug that prevents crow from compiling with C++11. Please update GCC to > 8.3 or use C++ > 11."
+#error "GCC 8.1 - 8.3 has a bug that prevents crow from compiling with C++11. Please update GCC to > 8.3 or use C++ > 11."
 #endif
 #endif
