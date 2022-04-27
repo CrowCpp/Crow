dc988ce27ba1646e7ec4b636d04efbbf2ce1d8d5
oscar.chen <cloudssj3@gmail.com>
2022-04-26 20:33:10 -0700

    fix clang format

--- include/crow/websocket.h
+++ include/crow/websocket.h
@@ -400,7 +400,7 @@ namespace crow
                             close_connection_ = true;
                             adaptor_.close();
                             if (error_handler_)
-                              error_handler_(*this);
+                                error_handler_(*this);
                             check_destroy();
                         }
                         else if (has_mask_)
