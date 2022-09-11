e046137fd9614f3458e7b115ea1af21f15d8c3f9
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2022-09-11 23:17:33 +0100

    Moved mime validation and lookup into the response class as static methods
    This addresses PR concerns regarding the addition of mime_utils.h, where the
    functionality is only used within http_response.h.

--- include/crow/http_response.h
+++ include/crow/http_response.h
@@ -18,7 +18,7 @@
 #include "crow/ci_map.h"
 #include "crow/socket_adaptors.h"
 #include "crow/logging.h"
-#include "crow/mime_utils.h"
+#include "crow/mime_types.h"
 #include "crow/returnable.h"
 
 
@@ -64,6 +64,57 @@ namespace crow
         {
             return crow::get_header_value(headers, key);
         }
+
+        // naive validation of a mime-type string
+        static bool validate_mime_type(const std::string& candidate) noexcept
+        {
+            // Here we simply check that the candidate type starts with
+            // a valid parent type, and has at least one character afterwards.
+            std::array<std::string, 10> valid_parent_types = {
+              "application/", "audio/", "font/", "example/",
+              "image/", "message/", "model/", "multipart/",
+              "text/", "video/"};
+            for (const std::string& parent : valid_parent_types)
+            {
+                // ensure the candidate is *longer* than the parent,
+                // to avoid unnecessary string comparison and to
+                // reject zero-length subtypes.
+                if (candidate.size() <= parent.size())
+                {
+                    continue;
+                }
+                // strncmp is used rather than substr to avoid allocation,
+                // but a string_view approach would be better if Crow
+                // migrates to C++17.
+                if (strncmp(parent.c_str(), candidate.c_str(), parent.size()) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Find the mime type from the content type either by lookup,
+        // or by the content type itself, if it is a valid a mime type.
+        // Defaults to text/plain.
+        static std::string get_mime_type(const std::string& contentType)
+        {
+            const auto mimeTypeIterator = mime_types.find(contentType);
+            if (mimeTypeIterator != mime_types.end())
+            {
+                return mimeTypeIterator->second;
+            }
+            else if (validate_mime_type(contentType))
+            {
+                return contentType;
+            }
+            else
+            {
+                CROW_LOG_WARNING << "Unable to interpret mime type for content type '" << contentType << "'. Defaulting to text/plain.";
+                return "text/plain";
+            }
+        }
+
 
         // clang-format off
         response() {}
