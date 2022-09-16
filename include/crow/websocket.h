af4bdac314cce06308ba06adb6fb1af604a9119c
Ryan Russell <git@ryanrussell.org>
2022-09-16 07:03:46 -0500

    refactor(websocket): readability improvements
    Signed-off-by: Ryan Russell <git@ryanrussell.org>

--- include/crow/websocket.h
+++ include/crow/websocket.h
@@ -426,7 +426,7 @@ namespace crow
                             close_connection_ = true;
                             adaptor_.close();
                             if (error_handler_)
-                                error_handler_(*this, "Message length exceeds maximum paylaod.");
+                                error_handler_(*this, "Message length exceeds maximum payload.");
                             check_destroy();
                         }
                         else if (has_mask_)
@@ -612,7 +612,7 @@ namespace crow
             /// Send the buffers' data through the socket.
 
             ///
-            /// Also destroyes the object if the Close flag is set.
+            /// Also destroys the object if the Close flag is set.
             void do_write()
             {
                 if (sending_buffers_.empty())
@@ -678,7 +678,7 @@ namespace crow
             uint16_t mini_header_;
             bool has_sent_close_{false};
             bool has_recv_close_{false};
-            bool error_occured_{false};
+            bool error_occurred_{false};
             bool pong_received_{false};
             bool is_close_handler_called_{false};
 
