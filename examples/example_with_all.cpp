befdcd34de3bb9eff321ab8dc1cbdaf04a64e3d7
Laguna Du <lag945@gmail.com>
2022-10-21 16:08:38 +0800

    fix post method.

--- examples/example_with_all.cpp
+++ examples/example_with_all.cpp
@@ -81,6 +81,7 @@ int main()
 
     // more json example
     CROW_ROUTE(app, "/add_json")
+	.methods("POST"_method)
     ([](const crow::request& req) {
         auto x = crow::json::load(req.body);
         if (!x)
