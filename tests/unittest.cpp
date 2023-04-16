e4e916a094c1ff640436fd03d4cf4ecddc5f58d9
sehe <sgheeren@gmail.com>
2023-04-16 06:06:30 +0200

    Fix broken unit test

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -2237,7 +2237,7 @@ TEST_CASE("websocket_missing_host")
     SimpleApp app;
 
     CROW_ROUTE(app, "/ws").websocket()
-      .onaccept([&](const crow::request& req, void**) {
+      .onaccept([&](const crow::request& req) {
           CROW_LOG_INFO << "Accepted websocket with URL " << req.url;
           return true;
       })
