f3e0846ee5f8bc484bc2bc393c4aefd7fc3ed627
jfm <jfm@nowhere.com>
2023-12-29 12:52:10 +0100

    My mistake. Crow is compiled with c++11 under windows and has a macro for std::min not being defined as std::min(a,b) under c++11, CROW_MIN

--- include/crow/http_connection.h
+++ include/crow/http_connection.h
@@ -442,7 +442,7 @@ namespace crow
                     size_t length = res.body.length();
                     for(size_t transferred = 0; transferred < length;)
                     {
-                        size_t to_transfer = std::min(16384UL, length-transferred);
+                        size_t to_transfer = CROW_MIN(16384UL, length-transferred);
                         buffers[0] = asio::const_buffer(data+transferred, to_transfer);
                         do_write_sync(buffers);
                         transferred += to_transfer;
