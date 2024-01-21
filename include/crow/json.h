8bb7cf15b520e06bd63f23b0ceb641e149480699
June Han <jun_h@pretia.co.jp>
2024-01-21 22:22:34 +0900

    fix: abandon one-time macro

--- include/crow/json.h
+++ include/crow/json.h
@@ -1851,11 +1851,10 @@ namespace crow
                             } f_state;
                             char outbuf[128];
 #ifdef _MSC_VER
-#define MSC_COMPATIBLE_SPRINTF(BUFFER_PTR, FORMAT_PTR, VALUE) sprintf_s((BUFFER_PTR), 128, (FORMAT_PTR), DBL_DECIMAL_DIG, (VALUE))
+                            sprintf_s(outbuf, 128, "%.*g", DBL_DECIMAL_DIG, v.num.d);
 #else
-#define MSC_COMPATIBLE_SPRINTF(BUFFER_PTR, FORMAT_PTR, VALUE) sprintf((BUFFER_PTR), (FORMAT_PTR), DECIMAL_DIG, (VALUE))
+                            sprintf(outbuf, "%.*g", DECIMAL_DIG, v.num.d);
 #endif
-                            MSC_COMPATIBLE_SPRINTF(outbuf, "%.*g", v.num.d);
                             char *p = &outbuf[0], *o = nullptr; // o is the position of the first trailing 0
                             f_state = start;
                             while (*p != '\0')
