5af2513d31fb454ca53ae871331a03900c63b9dd
fliiiix <hi@l33t.name>
2024-08-30 10:26:20 +0200

    Enable suggest-override warning and fix instances

--- include/crow/mustache.h
+++ include/crow/mustache.h
@@ -54,7 +54,7 @@ namespace crow // NOTE: Already documented in "crow/app.h"
             invalid_template_exception(const std::string& msg):
               msg("crow::mustache error: " + msg)
             {}
-            virtual const char* what() const throw()
+            virtual const char* what() const throw() override
             {
                 return msg.c_str();
             }
