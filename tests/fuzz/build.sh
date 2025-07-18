a382bd3e9f20a60fdc04f5ed724a563b70c40652
Adam Korczynski <adam@adalogics.com>
2025-07-18 22:36:35 +0100

    update OSS-Fuzz build script
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

--- tests/fuzz/build.sh
+++ tests/fuzz/build.sh
@@ -1,8 +1,7 @@
-cd $SRC/crow
-mkdir -p build
-cmake -S . -B build -DCROW_BUILD_FUZZER=ON -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF && cmake --build build --target install
-
-# Build the corpora
-cd tests/fuzz
+mkdir -p build && cd build
+cmake .. -DCROW_BUILD_FUZZER=ON -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=ON
+cmake --build . --target install
+make -j$(nproc)
+cd ../tests/fuzz
 zip -q $OUT/template_fuzzer_seed_corpus.zip template_corpus/*
 zip -q $OUT/request_fuzzer_seed_corpus.zip html_corpus/*
