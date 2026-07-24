c05fd5e39092951c2d026a092bb932f84db27e10
Janusz <januszvdm@gmail.com>
2026-07-24 12:10:07 +0200

    #1077 feature: Add TCP_NODELAY option tests for HTTP and WebSocket connections

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -2991,6 +2991,22 @@ TEST_CASE("TCP_NODELAY_http_api_enabled")
     app.tcp_nodelay(true);
     auto http_options = app.tcp_socket_options();
     CHECK(http_options.no_delay == true);
+
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context,
+                                     asio::ip::tcp::endpoint(asio::ip::make_address(LOCALHOST_ADDRESS), 0));
+
+    asio::ip::tcp::socket client_socket(io_context);
+    client_socket.connect(acceptor.local_endpoint());
+
+    asio::ip::tcp::socket server_socket(io_context);
+    acceptor.accept(server_socket);
+
+    crow::detail::socket::apply_tcp_socket_options(server_socket, http_options);
+
+    asio::ip::tcp::no_delay no_delay;
+    server_socket.get_option(no_delay);
+    CHECK(no_delay.value());
 }
 
 // Tests that HTTP socket TCP_NODELAY can be disabled via tcp_nodelay(false) API
@@ -3009,6 +3025,22 @@ TEST_CASE("TCP_NODELAY_http_api_disabled")
     app.tcp_nodelay(false);
     auto http_options = app.tcp_socket_options();
     CHECK(http_options.no_delay == false);
+
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context,
+                                     asio::ip::tcp::endpoint(asio::ip::make_address(LOCALHOST_ADDRESS), 0));
+
+    asio::ip::tcp::socket client_socket(io_context);
+    client_socket.connect(acceptor.local_endpoint());
+
+    asio::ip::tcp::socket server_socket(io_context);
+    acceptor.accept(server_socket);
+
+    crow::detail::socket::apply_tcp_socket_options(server_socket, http_options);
+
+    asio::ip::tcp::no_delay no_delay;
+    server_socket.get_option(no_delay);
+    CHECK(!no_delay.value());
 }
 
 // Tests that WebSocket socket TCP_NODELAY defaults to false (disabled)
@@ -3042,6 +3074,22 @@ TEST_CASE("TCP_NODELAY_websocket_api_enabled")
     app.websocket_tcp_nodelay(true);
     auto ws_options = app.websocket_tcp_socket_options();
     CHECK(ws_options.no_delay == true);
+
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context,
+                                     asio::ip::tcp::endpoint(asio::ip::make_address(LOCALHOST_ADDRESS), 0));
+
+    asio::ip::tcp::socket client_socket(io_context);
+    client_socket.connect(acceptor.local_endpoint());
+
+    asio::ip::tcp::socket server_socket(io_context);
+    acceptor.accept(server_socket);
+
+    crow::detail::socket::apply_tcp_socket_options(server_socket, ws_options);
+
+    asio::ip::tcp::no_delay no_delay;
+    server_socket.get_option(no_delay);
+    CHECK(no_delay.value());
 }
 
 // Tests that WebSocket socket TCP_NODELAY can be disabled via websocket_tcp_nodelay(false) API
@@ -3060,6 +3108,22 @@ TEST_CASE("TCP_NODELAY_websocket_api_disabled")
     app.websocket_tcp_nodelay(false);
     auto ws_options = app.websocket_tcp_socket_options();
     CHECK(ws_options.no_delay == false);
+
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context,
+                                     asio::ip::tcp::endpoint(asio::ip::make_address(LOCALHOST_ADDRESS), 0));
+
+    asio::ip::tcp::socket client_socket(io_context);
+    client_socket.connect(acceptor.local_endpoint());
+
+    asio::ip::tcp::socket server_socket(io_context);
+    acceptor.accept(server_socket);
+
+    crow::detail::socket::apply_tcp_socket_options(server_socket, ws_options);
+
+    asio::ip::tcp::no_delay no_delay;
+    server_socket.get_option(no_delay);
+    CHECK(!no_delay.value());
 }
 
 // Smoke test to verify HTTP server starts and handles requests correctly
