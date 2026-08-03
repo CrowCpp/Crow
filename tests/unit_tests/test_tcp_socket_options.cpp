37503d26e832eede6a529929a9dc5d1577f37f4c
Janusz <januszvdm@gmail.com>
2026-08-03 19:15:58 +0200

    Fix error_code type alias to asio_error_code for consistency in socket options

--- tests/unit_tests/test_tcp_socket_options.cpp
+++ tests/unit_tests/test_tcp_socket_options.cpp
@@ -11,9 +11,9 @@ using namespace crow;
 
 #ifdef CROW_USE_BOOST
 namespace asio = boost::asio;
-using error_code = boost::system::error_code;
+using asio_error_code = boost::system::error_code;
 #else
-using error_code = asio::error_code;
+using asio_error_code = asio::error_code;
 #endif
 
 namespace
@@ -195,7 +195,7 @@ namespace
 
     bool can_open_and_apply_v6_only(bool enabled)
     {
-        error_code ec;
+        asio_error_code ec;
         asio::io_context io_context;
         asio::ip::tcp::acceptor acceptor(io_context);
         acceptor.open(asio::ip::tcp::v6(), ec);
