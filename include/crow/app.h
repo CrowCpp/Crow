e75b19676de79c9df7585d72fbf87c80aacf02b8
gittiver <gulliver@traumkristalle.net>
2026-09-14 14:20:07 +0200

    Update server constructors to include max_connections

--- include/crow/app.h
+++ include/crow/app.h
@@ -666,7 +666,7 @@ namespace crow
                 }
                 tcp::endpoint endpoint(addr, port_);
                 router_.using_ssl = true;
-                ssl_server_ = std::make_unique<ssl_server_t>(this, endpoint, server_name_, &middlewares_, concurrency_, timeout_, &ssl_context_, tcp_socket_options_);
+                ssl_server_ = std::make_unique<ssl_server_t>(this, endpoint, server_name_, &middlewares_, concurrency_, max_connections_, timeout_, &ssl_context_, tcp_socket_options_);
                 ssl_server_->set_tick_function(tick_interval_, tick_function_);
                 ssl_server_->signal_clear();
                 for (auto snum : signals_)
@@ -682,7 +682,7 @@ namespace crow
                 if (use_unix_)
                 {
                     UnixSocketAcceptor::endpoint endpoint(bindaddr_);
-                    unix_server_ = std::make_unique<unix_server_t>(this, endpoint, server_name_, &middlewares_, concurrency_, timeout_, nullptr);
+                    unix_server_ = std::make_unique<unix_server_t>(this, endpoint, server_name_, &middlewares_, concurrency_, max_connections_, timeout_, nullptr);
                     unix_server_->set_tick_function(tick_interval_, tick_function_);
                     for (auto snum : signals_)
                     {
@@ -700,7 +700,7 @@ namespace crow
                         return;
                     }
                     TCPAcceptor::endpoint endpoint(addr, port_);
-                    server_ = std::make_unique<server_t>(this, endpoint, server_name_, &middlewares_, concurrency_, timeout_, nullptr, tcp_socket_options_);
+                    server_ = std::make_unique<server_t>(this, endpoint, server_name_, &middlewares_, concurrency_, max_connections_, timeout_, nullptr, tcp_socket_options_);
                     server_->set_tick_function(tick_interval_, tick_function_);
                     for (auto snum : signals_)
                     {
